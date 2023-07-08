8040a86d42f0600fb89e11c6c4f0e7cd4911f852
OmarNasserr <omarnasserr44@gmail.com>
2023-07-08 10:24:57 +0300

    (UPDT) playground_app

--- BookyEnv/Booky/playground_app/urls.py
+++ BookyEnv/Booky/playground_app/urls.py
@@ -1,8 +1,8 @@
 from urllib import request
 from django.urls import path,include
-from playground_app.views.POST_requests import PlaygroundCreate
-from playground_app.views.GET_requests import PlaygroundList,NearestPLaygrounds
-from playground_app.views.PUT_DELETE_requests import PlaygroundDetail
+from .views.POST_requests import PlaygroundCreate
+from .views.GET_requests import PlaygroundList,NearestPLaygrounds
+from .views.PUT_DELETE_requests import PlaygroundDetail
 
 urlpatterns = [
     path('create/',PlaygroundCreate.as_view(),name='playground-create'),
