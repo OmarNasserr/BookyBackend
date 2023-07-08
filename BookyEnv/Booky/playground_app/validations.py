8b7c63a32d9397088235aa937413c8ef58793663
OmarNasserr <omarnasserr44@gmail.com>
2023-07-08 10:24:48 +0300

    (UPDT) playground_app

--- BookyEnv/Booky/playground_app/validations.py
+++ BookyEnv/Booky/playground_app/validations.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 import re
+from helper_files.status_code import Status_code
 
 
 class PlaygroundAppValidations():
@@ -11,31 +12,31 @@ class PlaygroundAppValidations():
         if valid:
             if len(data['p_name']) < 3:
                 return Response(data={'message': "Playground's name can't be less than three characters",
-                                      'status':status.HTTP_400_BAD_REQUEST},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                      'status':Status_code.bad_request},
+                                status=Status_code.bad_request)
             if not re.match(h_m_s_regex,data['open_time']):
                 return Response(data={'message': "Playground's open_time doesn't match time H:M:S format",
-                                      'status':status.HTTP_400_BAD_REQUEST},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                      'status':Status_code.bad_request},
+                                status=Status_code.bad_request)
             if not re.match(h_m_s_regex,data['close_time']):
                 return Response(data={'message': "Playground's close_time doesn't match time H:M:S format",
-                                      'status':status.HTTP_400_BAD_REQUEST},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                      'status':Status_code.bad_request},
+                                status=Status_code.bad_request)
             if len(data['description']) < 10:
                 return Response(data={'message': "Playground's description can't be less than 10 characters",
-                                      'status':status.HTTP_400_BAD_REQUEST},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                      'status':Status_code.bad_request},
+                                status=Status_code.bad_request)
             if float(data['price_per_hour']) < 10.0:
                 return Response(data={'message': "Playground's price can't be less than 10 pounds",
-                                      'status':status.HTTP_400_BAD_REQUEST},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                      'status':Status_code.bad_request},
+                                status=Status_code.bad_request)
             else:
                 return Response(data={"message": "Playground was added successfully.",
-                                      'status':status.HTTP_201_CREATED},
-                                status=status.HTTP_201_CREATED)
+                                      'status':Status_code.created},
+                                status=Status_code.created)
         else:
-            return Response(data={'message':str(err),"status":status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={'message':str(err),"status":Status_code.bad_request},
+                                    status=Status_code.bad_request)
     
     def validate_playground_update(data,valid,err):
         if valid:
@@ -50,32 +51,32 @@ class PlaygroundAppValidations():
             if 'p_name' in data.keys():
                 if len(data['p_name'])!=0 and len(data['p_name'])< 3:
                     return Response(data={'message': "Playground's name can't be less than three characters",
-                                            'status':status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                                            'status':Status_code.bad_request},
+                                    status=Status_code.bad_request)
             if 'open_time' in data.keys():      
                 if not re.match(h_m_s_regex,data['open_time']):
                     return Response(data={'message': "Playground's open_time doesn't match time H:M:S format",
-                                            'status':status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                                            'status':Status_code.bad_request},
+                                    status=Status_code.bad_request)
             if 'close_time' in data.keys():
                 if not re.match(h_m_s_regex,data['close_time']):
                     return Response(data={'message': "Playground's close_time doesn't match time H:M:S format",
-                                            'status':status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                                            'status':Status_code.bad_request},
+                                    status=Status_code.bad_request)
             if 'description' in data.keys():
                 if len(data['description']) < 10:
                     return Response(data={'message': "Playground's description can't be less than 10 characters",
-                                            'status':status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                                            'status':Status_code.bad_request},
+                                    status=Status_code.bad_request)
             if 'price_per_hour' in data.keys():       
                 if float(data['price_per_hour']) < 10.0:
                     return Response(data={'message': "Playground's price can't be less than 10 pounds",
-                                            'status':status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                                            'status':Status_code.bad_request},
+                                    status=Status_code.bad_request)
             
             return Response(data={"message": "Playground was updated successfully.",
-                                    'status':status.HTTP_202_ACCEPTED},
-                            status=status.HTTP_202_ACCEPTED)
+                                    'status':Status_code.updated},
+                            status=Status_code.updated)
         else:
-            return Response(data={'message':str(err),"status":status.HTTP_400_BAD_REQUEST},
-                                    status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={'message':str(err),"status":Status_code.bad_request},
+                                    status=Status_code.bad_request)
